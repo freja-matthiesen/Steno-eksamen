626a47f120facd0ecd43ca7ddf39a4ea0f9a2b5a
linegj <linekbv@gmail.com>
2025-05-18 15:01:02 +0200

    kommentarer

--- js/quiz.js
+++ js/quiz.js
@@ -1,79 +1,82 @@
 'use-strict'
 
-const questions = [
+const questions = [ // Array med objekter. Indehodler spørgsmål og korrekte svar 
     {
-        question: "Hvilket nr illusterer social angst?", 
+        question: "Hvilket nr illustrerer social angst?", // spørgsmål 1
         answers: [
             { text: "1", correct: false },
-            { text: "2", correct: true },
+            { text: "2", correct: true }, // korrekt svar
             { text: "3", correct: false },
             { text: "4", correct: false }
         ]
     },
     {
-        question: "Hvilket nr illusterer fobi angst?", 
+        question: "Hvilket nr illustrerer fobi angst?", // spørgsmål 2
         answers: [
             { text: "1", correct: false },
             { text: "2", correct: false },
             { text: "3", correct: false },
-            { text: "4", correct: true }
+            { text: "4", correct: true } // korrekt svar
         ]
     },
     {
-        question: "Hvilket nr illusterer seperations angst?", 
+        question: "Hvilket nr illustrerer seperations angst?", // spørgsmål 3
         answers: [
-            { text: "1", correct: true },
+            { text: "1", correct: true }, //korrekt svar
             { text: "2", correct: false },
             { text: "3", correct: false },
             { text: "4", correct: false }
         ]
     },
     {
-        question: "Hvilket nr illusterer generaliseret angst?", 
+        question: "Hvilket nr illustrerer generaliseret angst?",  // spørgsmål 4
         answers: [
             { text: "1", correct: false },
             { text: "2", correct: false },
-            { text: "3", correct: true },
+            { text: "3", correct: true }, //korrekt svar
             { text: "4", correct: false }
         ]
     }
 ];
 
+// Opretter 3 konstant variabler, som henter elementer fra HTML-dokumentet
 const questionElement = document.getElementById('question');
 const answerButtons = document.getElementById('answer-buttons');
 const nextButton = document.getElementById('next-btn');
 
-let currentQuestionIndex = 0;
-let score = 0;
+let currentQuestionIndex = 0; // Variablen holder styr på hvilket spørgsmål der vises
+let score = 0; // Variablen holder styr på hvor mange rigtige svar der er givet
 
+// StartQuiz funktionen starter quiz på spg 1 og sætter score til 0
 function startQuiz () {
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML = "Næste";
-    showQuestion();
+    nextButton.innerHTML = "Næste"; 
+    showQuestion(); // kalder funktionen showQuestion, som viser spørgsmål
 }
+
 
 function showQuestion () {
     resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    let currentQuestion = questions[currentQuestionIndex]; // henter spørgsmål fra arrayet baseret på index
+    let questionNo = currentQuestionIndex + 1; // første spg er 1 og ikke 0
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question; // opdaterer visning af spørgsmål i questionElement
 
-    currentQuestion.answers.forEach(answer => {
+    currentQuestion.answers.forEach(answer => { // forEach loopet --> opretter knapper for hvert svar
         const button = document.createElement('button');
-        button.innerText = answer.text;
-        button.classList.add('btn');
-        answerButtons.appendChild(button);
-        if (answer.correct) {
+        button.innerText = answer.text; 
+        button.classList.add('btn'); 
+        answerButtons.appendChild(button); //tilføjer knappen 'button' som barn til answerButtons
+        if (answer.correct) { // hvis svaret er korrekt, tilføjes data attributten correct
             button.dataset.correct = answer.correct;
         }
-        button.addEventListener('click', selectAnswer);
+        button.addEventListener('click', selectAnswer); // når der klikkes på knappen, kaldes funktionen selectAnswer
     });
 }
 
-function resetState () {
+function resetState () { // nulstiller qiuz og fjerner tidligere svar
     nextButton.style.display = 'none';
-    while (answerButtons.firstChild) {
+    while (answerButtons.firstChild) { // loopet kører så længe der er mindst et barn i answerButtons
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
@@ -81,11 +84,11 @@ function resetState () {
 function selectAnswer (e) {
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
-    if (isCorrect) {
+    if (isCorrect) { // hvis svaret er korrekt, tilføjes klassen korrekt og scoren øges med 1
         selectedBtn.classList.add('korrekt');
-        score++;
-    } else {
-        selectedBtn.classList.add('forkert');
+        score++; 
+    } else { // hvis svaret er forkert, tilføjes klassen forkert
+        selectedBtn.classList.add('forkert'); 
     }
     Array.from(answerButtons.children).forEach(button => {
         if (button.dataset.correct === "true") {
@@ -97,7 +100,7 @@ function selectAnswer (e) {
 
 }
 
-function showScore () {
+function showScore () { // viser score og giver mulighed for at starte quizzen forfra
     resetState();
     questionElement.innerHTML = `Du fik ${score} ud af ${questions.length} rigtige!`;
     nextButton.innerHTML = "Prøv igen";
@@ -121,4 +124,4 @@ nextButton.addEventListener('click', () => {
     }
 });
 
-startQuiz();
+startQuiz(); // kalder funktionen
